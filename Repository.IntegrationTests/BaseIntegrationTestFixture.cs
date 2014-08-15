7c705f0ab09ef5e5ac397cb5754912d0e42eaafb
Balasubramanian N <balasubn@thoughtworks.com>
2014-08-15 20:16:16 +0530

    Added framework for Integration tests for Repositories - [Bala]

---
+++ Repository.IntegrationTests/BaseIntegrationTestFixture.cs
@@ -0,0 +1,35 @@
+﻿using System.Transactions;
+
+using DataAccessLayer;
+
+using NUnit.Framework;
+
+namespace Repository.IntegrationTests
+{
+    [TestFixture]
+    public class BaseIntegrationTestFixture
+    {
+        protected OnlineJudgeContext OnlineJudgeContext;
+
+        protected TransactionScope TransactionScope;
+
+        [TestFixtureSetUp]
+        public void InitialiseDbContext()
+        {
+            this.OnlineJudgeContext = new OnlineJudgeContext();
+            this.OnlineJudgeContext.Database.CreateIfNotExists();
+        }
+
+        [SetUp]
+        public void BeginTransaction()
+        {
+            this.TransactionScope = new TransactionScope(TransactionScopeOption.RequiresNew);
+        }
+
+        [TearDown]
+        public void EndTransaction()
+        {
+            this.TransactionScope.Dispose();
+        }
+    }
+}
